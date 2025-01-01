6c9e6a7ddcac08af92f4a1f1d1567096dba36eb4
joon <126473+dvessel@users.noreply.github.com>
2025-01-01 06:10:41 -0500

    Make more generic, optional pre/postprocess scripts.
    There should only be 3 things that differentiates each shortcut.
    1. shortcut.icns based on boxart or screen shot. each will be checked based on availability.
    2. `$gpath` game rom path.
    3. `$cpath` core dylib path.
    
    Optional pre/post scripts I wanted since RetroArch doesn't support resolution changes on MacOS.

---
+++ Mushihime-Sama Futari Black Label - Another Ver (2009:11:27 INTERNATIONAL BL).app/Contents/Resources/postprocess.sh
@@ -0,0 +1,6 @@
+ #!/usr/bin/env zsh
+
+# Revert resolution back to "More Space".
+
+# [ mode 72: res:2056x1329 hz:120 color_depth:8 scaling:on ]
+# /opt/homebrew/bin/displayplacer "id:1 mode:72"
---
+++ Mushihime-Sama Futari Black Label - Another Ver (2009:11:27 INTERNATIONAL BL).app/Contents/Resources/preprocess.sh
@@ -0,0 +1,11 @@
+ #!/usr/bin/env zsh
+
+# Set native display resolution to prevent shader artifacts.
+# This is very specific to my 16" MacBook Pro.
+#
+# It is not required but if you normally set the display scaling to "More Space"
+# which is not an even scaling, install through `brew install displayplacer` and
+# find the display mode with `displayplacer list`.
+
+# [ mode 54: res:1728x1117 hz:120 color_depth:8 scaling:on ]
+# /opt/homebrew/bin/displayplacer "id:1 mode:54"
