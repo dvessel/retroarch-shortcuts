e8a198730d829d0ee25ad6c162e62363fc2f892a
joon <126473+dvessel@users.noreply.github.com>
2025-02-11 06:01:45 -0500

    Redo makeicnset js for clarity + error handling.

--- makeicnset.js
+++ makeicnset.js
@@ -1,7 +1,6 @@
-
-
-// Define all required icon sizes and their filenames
+// Define all required icon sizes and their filenames.
 // https://developer.apple.com/design/human-interface-guidelines/app-icons#macOS-app-icon-sizes
+//
 // +---------------------+------------+
 // | filename            | resolution |
 // +---------------------+------------+
@@ -16,39 +15,51 @@
 // | icon_512x512.png    | 512x512    |
 // | icon_512x512@2x.png | 1024x1024  |
 // +---------------------+------------+
+//
+// For use with sips, "scriptable image processing system".
+// Unofficial Documentation: https://manicmaniac.github.io/sips-js-api/
 
-const iconSizes = [
-  { name: 'icon_16x16.png',      length: 16,   shadowBlur: 0 },
-  { name: 'icon_16x16@2x.png',   length: 32,   shadowBlur: 0 },
-  { name: 'icon_32x32.png',      length: 32,   shadowBlur: 0 },
-  { name: 'icon_32x32@2x.png',   length: 64,   shadowBlur: 0 },
-  { name: 'icon_128x128.png',    length: 128,  shadowBlur: 2 },
-  { name: 'icon_128x128@2x.png', length: 256,  shadowBlur: 4 },
-  { name: 'icon_256x256.png',    length: 256,  shadowBlur: 4 },
-  { name: 'icon_256x256@2x.png', length: 512,  shadowBlur: 8 },
-  { name: 'icon_512x512.png',    length: 512,  shadowBlur: 10 },
-  { name: 'icon_512x512@2x.png', length: 1024, shadowBlur: 20 }
+const iconConfig = [
+  { name: 'icon_16x16.png',      size: 16,   shadowBlur: 0 },
+  { name: 'icon_16x16@2x.png',   size: 32,   shadowBlur: 0 },
+  { name: 'icon_32x32.png',      size: 32,   shadowBlur: 0 },
+  { name: 'icon_32x32@2x.png',   size: 64,   shadowBlur: 0 },
+  { name: 'icon_128x128.png',    size: 128,  shadowBlur: 2 },
+  { name: 'icon_128x128@2x.png', size: 256,  shadowBlur: 4 },
+  { name: 'icon_256x256.png',    size: 256,  shadowBlur: 4 },
+  { name: 'icon_256x256@2x.png', size: 512,  shadowBlur: 10 },
+  { name: 'icon_512x512.png',    size: 512,  shadowBlur: 10 },
+  { name: 'icon_512x512@2x.png', size: 1024, shadowBlur: 20 }
 ]
 
-iconSizes.forEach(img => {
+iconConfig.forEach(cfg => {
+  try {
 
-  const scaled = sips.images[0].scaledSizeWithLongestEdge(img.length-(img.shadowBlur*2))
-  const canvas = new Canvas(img.length, img.length)
+    const canvas = new Canvas(cfg.size, cfg.size)
+    canvas.shadowColor = 'rgba(0, 0, 0, 0.6)'
+    canvas.shadowBlur = cfg.shadowBlur
+    canvas.shadowOffsetY = -cfg.shadowBlur / 2 // Downward offset at half the blur radius.
 
-  canvas.shadowColor = 'rgba(0, 0, 0, 0.6)'
-  canvas.shadowBlur = img.shadowBlur
-  canvas.shadowOffsetY = -img.shadowBlur/2
+    // sips.images is an array.
+    const image = sips.images.pop()
 
-  canvas.drawImage(
-    sips.images[0],
-   (canvas.width-scaled.width)/2,
-   (canvas.height-scaled.height)-(img.shadowBlur*1.5),
-    scaled.width,
-    scaled.height
-  )
+    // Scale image to fit within the canvas minus 2x shadow blur to avoid clipping.
+    const imageDimension = image.scaledSizeWithLongestEdge(cfg.size - (cfg.shadowBlur * 2))
 
-  const output = new Output(canvas, sips.outputPath)
-  output.name = img.name
-  output.addToQueue()
+    // Center horizontally and bottom vertically with space for blurring.
+    canvas.drawImage(
+      image,
+      (canvas.width - imageDimension.width) / 2,
+      (canvas.height - imageDimension.height) - (cfg.shadowBlur * 1.5),
+      imageDimension.width,
+      imageDimension.height
+    )
 
+    const output = new Output(canvas, sips.outputPath)
+    output.name = cfg.name
+    output.addToQueue()
+
+  } catch (error) {
+    print(`Error processing ${cfg.name}:\n - ${error}`)
+  }
 })
